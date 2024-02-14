33d214c20a6972f26a168a79acdfe73720f2c932
viswanath <108261532+viswanath07@users.noreply.github.com>
2024-02-14 11:44:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="index.css" rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-        <title>Virtual Valentine's Day Card</title>
-    </head>
-    <body>
-        <div class="envelope-wrapper">
-            <div id="envelope" class="close">
-                <div class="front flap"></div>
-                <div class="front pocket"></div>
-                <div class="letter">
-                    <p class="words line1">Hello there,</p>
-                    <p class="words line2">You're the CSS to my HTML! &#128540;</p>
-                    <p class="words line3">Love,</p>
-                    <p class="words line4">Viswa</p>
-                </div>
-                <div class="hearts">
-                    <div class="heart a1"></div>
-                    <div class="heart a2"></div>
-                    <div class="heart a3"></div>
-                </div>
-            </div>
-        </div>
-        <div class="reset">
-            <button id="open">Open</button>
-            <button id="reset">Reset</button>
-        </div>
-        <h1 class="valentine">Happy Valentine's Day!</h1>
-        <script src="index.js"></script>
-    </body>
-</html>
+$(document).ready(function() {
+    var envelope = $("#envelope");
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
+
+    envelope.click(function() {
+        open();
+    });
+    btn_open.click(function() {
+        open();
+    });
+    btn_reset.click(function() {
+        close();
+    });
+
+    function open() {
+        envelope.addClass("open")
+            .removeClass("close");
+    }
+
+    function close() {
+        envelope.addClass("close")
+            .removeClass("open");
+    }
+
+})
